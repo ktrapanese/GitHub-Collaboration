afdbbb20ef628282dc887832c73b88cc0aa29579
ktrapanese <ktrapanese@madeira.org>
2021-12-17 13:13:22 -0500

    added a few more comments to help understand code.

--- Game_code.py
+++ Game_code.py
@@ -7,15 +7,15 @@ def welcome():
     print('Welcome to "The Impossible Game", named so because only 0.1% of people are able to win...So not impossible, probability speaking, but you get the point! The game is simple, I will ask you one simple question and you will give me one simple answer. Got it? Okay, let us begin.')
 
 
-def goodbyes(actual_name: str)->str: 
+def goodbyes(actual_name: str)->str:  #function with parameters
     '''This function will print random goodbye form the list of goodbyes when the game is over. '''
     goodbyes: list[str] = ["Goodbye", "Adios", "I bid you adieu", "Well...see you later"] #list of goodbyes
     goodbye = (goodbyes[random.randint(0,3)])
     print(goodbye + " " + actual_name)
-    return goodbye + " " + actual_name
+    return goodbye + " " + actual_name #function with return values 
 
 
-def game():
+def game(): #function with boolean operator and while loops. 
     '''This function will be the game part of the code.  It will ask the question over and over again until the player answers "No".  It will then say bye to the user.'''
     print('Welcome to "The Impossible Game", named so because only 0.1% of people are able to win...So not impossible, probability speaking, but you get the point! The game is simple, I will ask you one simple question and you will give me one simple answer. Got it? Okay, let us begin.')
     answer = str(input("What is your name? "))
